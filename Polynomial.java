437e497dcdaf20569d1407bca37b94ebf74c5a33
Julian Liu <jul14nl1u@gmail.com>
2023-09-28 17:08:58 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,39 +1,79 @@
+import java.rmi.AccessException;
+
 public class Polynomial {
     
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial() {
         this.coefficients = new double[] {0};
+        this.exponents = new int[] {0};
     }
 
-    public Polynomial(double[] givenCoefficients) {
+    public Polynomial(double[] givenCoefficients, int[] givenExponents) {
         this.coefficients = givenCoefficients;
+        this.exponents = givenExponents;
     }
 
     public Polynomial add(Polynomial p) {
-        if (p.coefficients.length > this.coefficients.length) {
-            for (int i=0; i<this.coefficients.length; i++) {
-                p.coefficients[i] = p.coefficients[i] + this.coefficients[i];
-            }
-            this.coefficients = p.coefficients;
-        } else {
-            for (int i=0; i<p.coefficients.length; i++) {
-                this.coefficients[i] = this.coefficients[i] + p.coefficients[i];
+
+        // find largest exponent between input and calling polynomials
+        int max = 0;
+        for (int i = 0; i<p.exponents.length; i++) {
+            if (p.exponents[i] > max) {
+                max = p.exponents[i];
             }
         }
+        for (int i = 0; i<this.exponents.length; i++) {
+            if (this.exponents[i] > max) {
+                max = this.exponents[i];
+            }
+        }
+
+        // declare and initialize new polynomial of max exponent
+        double[] qCoefficients = new double[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qCoefficients[i] = 0;
+        }
+        int[] qExponents = new int[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qExponents[i] = i;
+        }
+
+        // loop through input polynomial and add corresponding coefficients
+        for (int i = 0; i<p.exponents.length; i++) {
+            qCoefficients[p.exponents[i]] += p.coefficients[i];
+        }
+
+        // loop through calling polynomial and add corresponding coefficients
+        for (int i = 0; i<this.exponents.length; i++) {
+            qCoefficients[this.exponents[i]] += this.coefficients[i];
+        }
+
+        // set calling polynomial to q and return
+        this.coefficients = qCoefficients;
+        this.exponents = qExponents;
         return this;
     }
 
     public double evaluate(double x) {
         double sum = 0;
         for (int i=0; i<this.coefficients.length; i++) {
-            sum = sum + this.coefficients[i] * Math.pow(x,i);
+            sum = sum + this.coefficients[i] * Math.pow(x,this.exponents[i]);
         }
         return sum;
     }
 
     public boolean hasRoot(double x) {
         return (this.evaluate(x) == 0);
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        
+
+
+
+        return p;
     }
 
 }
