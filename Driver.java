abec9a2b9a915d2c1498f75e2a92d0f1542e26db
Julian Liu <jul14nl1u@gmail.com>
2023-10-01 23:57:01 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,30 @@
 public class Driver {
     public static void main(String [] args) {
+        /*
         Polynomial p = new Polynomial(); 
         System.out.println(p.evaluate(3));
         double[] c1 = {6,0,0,5};
         Polynomial p1 = new Polynomial(c1);
         double[] c2 = {0,-2,0,0,-9};
         Polynomial p2 = new Polynomial(c2); 
+        Polynomial s = p1.add(p2); 
+        System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+        if(s.hasRoot(1)) {
+            System.out.println("1 is a root of s");
+        } else {
+            System.out.println("1 is not a root of s");
+        }
+        */
+
+        // begin testing code for lab2
+        Polynomial p = new Polynomial(); 
+        System.out.println(p.evaluate(3));
+        double[] c1 = {6,0,5,0};
+        int[] e1 = {0,1,3,2};
+        Polynomial p1 = new Polynomial(c1,e1);
+        double[] c2 = {0,0,-9,0,-2};
+        int[] e2 = {0,2,4,3,1};
+        Polynomial p2 = new Polynomial(c2,e2); 
         Polynomial s = p1.add(p2); 
         System.out.println("s(0.1) = " + s.evaluate(0.1)); 
         if(s.hasRoot(1)) {
--- Polynomial.java
+++ Polynomial.java
@@ -1,33 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Polynomial {
     
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial() {
         this.coefficients = new double[] {0};
+        this.exponents = new int[] {0};
     }
 
-    public Polynomial(double[] givenCoefficients) {
+    public Polynomial(double[] givenCoefficients, int[] givenExponents) {
         this.coefficients = givenCoefficients;
+        this.exponents = givenExponents;
+    }
+
+    public Polynomial(File inputFile) {
+        try {
+            Scanner input = new Scanner(inputFile);
+            String line = input.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Polynomial add(Polynomial p) {
-        if (p.coefficients.length > this.coefficients.length) {
-            for (int i=0; i<this.coefficients.length; i++) {
-                p.coefficients[i] = p.coefficients[i] + this.coefficients[i];
-            }
-            this.coefficients = p.coefficients;
-        } else {
-            for (int i=0; i<p.coefficients.length; i++) {
-                this.coefficients[i] = this.coefficients[i] + p.coefficients[i];
-            }
+
+        // find largest exponent between input and calling polynomials
+        int max = maxInt(p.exponents);
+        int thisMax = maxInt(this.exponents);
+        if (max < thisMax) {
+            max = thisMax;
         }
+
+        // declare and initialize new polynomial of max exponent
+        double[] qCoefficients = new double[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qCoefficients[i] = 0;
+        }
+        int[] qExponents = new int[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qExponents[i] = i;
+        }
+
+        // loop through input polynomial and add corresponding coefficients
+        for (int i = 0; i<p.exponents.length; i++) {
+            qCoefficients[p.exponents[i]] += p.coefficients[i];
+        }
+
+        // loop through calling polynomial and add corresponding coefficients
+        for (int i = 0; i<this.exponents.length; i++) {
+            qCoefficients[this.exponents[i]] += this.coefficients[i];
+        }
+
+        // set calling polynomial to q and return
+        this.coefficients = qCoefficients;
+        this.exponents = qExponents;
         return this;
     }
 
     public double evaluate(double x) {
         double sum = 0;
         for (int i=0; i<this.coefficients.length; i++) {
-            sum = sum + this.coefficients[i] * Math.pow(x,i);
+            sum = sum + this.coefficients[i] * Math.pow(x,this.exponents[i]);
         }
         return sum;
     }
@@ -36,4 +74,83 @@ public class Polynomial {
         return (this.evaluate(x) == 0);
     }
 
+    private int maxInt(int[] input) {
+        int max = 0;
+        for (int i = 0; i<input.length; i++) {
+            if (input[i] > max) {
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
+    private Polynomial delZeroes(Polynomial p) {
+        int zeroCount = 0;
+        for (int i=0; i<p.coefficients.length; i++) {
+            if (p.coefficients[i] == 0) {
+                zeroCount++;
+            }
+        }
+
+        double[] qCoefficients = new double[p.coefficients.length-zeroCount];
+        for (int i = 0; i<zeroCount+1; i++) {
+            qCoefficients[i] = 0;
+        }
+        int[] qExponents = new int[p.coefficients.length-zeroCount];
+        for (int i = 0; i<zeroCount+1; i++) {
+            qExponents[i] = i;
+        }
+
+        int nonzeroCount = 0;
+        for (int i=0; i<zeroCount; i++) {
+            if (p.coefficients[i] != 0) {
+                qCoefficients[nonzeroCount] = p.coefficients[i];
+                qExponents[nonzeroCount] = p.exponents[i];
+                nonzeroCount++;
+            }
+        }
+
+        this.coefficients = qCoefficients;
+        this.exponents = qExponents;
+        return this;
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        
+        // declare and initialize new polynomial of degree (deg(p)+deg(this))
+        int max = maxInt(p.exponents) + maxInt(this.exponents);
+        double[] qCoefficients = new double[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qCoefficients[i] = 0;
+        }
+        int[] qExponents = new int[max+1];
+        for (int i = 0; i<max+1; i++) {
+            qExponents[i] = i;
+        }
+
+        // loop through input polynomial and multiply with each term in the calling polynomial
+        for (int i=0; i<p.exponents.length; i++) {
+            for (int j=0; j<this.exponents.length; j++) {
+                qCoefficients[p.exponents[i]+this.exponents[j]] += p.coefficients[i] * this.coefficients[j];
+            }
+        }
+
+        // set calling polynomial to q and return
+        this.coefficients = qCoefficients;
+        this.exponents = qExponents;
+        return this;
+    }
+
+    public void saveToFile(String filename) {
+        try {
+			PrintStream output = new PrintStream(filename);
+            String out = new String(this.coefficients[0] + "x" + this.exponents[0]);
+            for (int i=1; i<this.coefficients.length; i++) {
+                out = out + "+" + this.coefficients[i] + "x" + this.exponents[i];
+            }
+			output.println(out);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+    }
 }
